6bca529ca1e7b23adccfbc64dcccf1c56e4b94bf
Moni Reak <contactdochii@gmail.com>
2023-12-07 17:49:48 +0700

    edit config.py v2

--- sql_app/config.py
+++ sql_app/config.py
@@ -1,5 +1,6 @@
 # config.py - is used to handle environment variable
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import ValidationError
 
 
 class Settings(BaseSettings):
@@ -12,8 +13,26 @@ class Settings(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
 
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+try:
+    Settings(
+        database_hostname="str",
+        database_port="str",
+        database_password="str",
+        database_name="str",
+        database_username="str",
+        secret_key="str",
+        algorithm="str",
+        access_token_expire_minutes=1,
+    )
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
+
     # tell pydantic to connect with the file name
     model_config = SettingsConfigDict(env_file=".env")
+
     # class Config:
     #     env_file = ".env"
 
