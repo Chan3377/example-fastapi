2e9231f6763d0fde1446f6fb0dd3df317550670a
Moni Reak <contactdochii@gmail.com>
2023-12-07 10:05:02 +0700

    add validation error in schemas

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ValidationError
 from typing import Optional
 from pydantic.types import conint
 
@@ -14,9 +14,21 @@ class PostBase(BaseModel):
     published: bool = True
 
 
+try:
+    PostBase()
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
+
+
 # create post Type
 class PostCreate(PostBase):
     pass
+
+
+try:
+    PostCreate()
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
 
 
 # response model by sending back user's email registration
@@ -28,6 +40,12 @@ class UserOut(BaseModel):
 
     class Config:
         orm_model = True
+
+
+try:
+    UserOut()
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
 
 
 # create response model back for "get post"
@@ -42,6 +60,12 @@ class Post(PostBase):
         orm_mode = True
 
 
+try:
+    Post()
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
+
+
 class PostOut(BaseModel):
     Post: Post
     votes: int
@@ -51,10 +75,22 @@ class PostOut(BaseModel):
         orm_mode = True
 
 
+try:
+    PostOut()
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
+
+
 # create user
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
+
+
+try:
+    UserCreate()
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
 
 
 class UserLogin(BaseModel):
@@ -62,16 +98,40 @@ class UserLogin(BaseModel):
     password: str
 
 
+try:
+    UserLogin()
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
+
+
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+try:
+    Token()
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
 
 
 class TokenData(BaseModel):
     id: Optional[int] = None
 
 
+try:
+    TokenData()
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
+
+
 # le: less than or equal to
 class Vote(BaseModel):
     post_id: int
     dir: conint(le=1)
+
+
+try:
+    Vote()
+except ValidationError as exc:
+    print(repr(exc.errors()[0]["type"]))
